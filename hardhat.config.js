b18cade1539486433bc22f3876ee2cc7dcf6e440
taiseidev <91307335+taiseidev@users.noreply.github.com>
2022-06-13 11:54:35 +0900

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+import "@nomiclabs/hardhat-waffle";
+export const solidity = "0.8.4";
+export const networks = {
+  rinkeby: {
+    url: "https url",
+    accounts: [
+      "seacret key",
+    ],
+  },
+};
